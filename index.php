dde3607e1670078a71caadd7819e3e7ad2ba8c58
nazar <nick20name17@gmail.com>
2023-02-08 15:44:21 +0200

    add markup and style for index page

--- index.php
+++ index.php
@@ -5,13 +5,34 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PhoneBook</title>
-    <link rel="stylesheet" href="assets/style.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PhoneBook</title>
+  <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
-    
+  <header class="header">
+    <div class="container header__container">
+      <h1 class="title">Phone Book</h1>
+      <a class="header__btn" href="">Log in</a>
+    </div>
+  </header>
+
+  <main>
+    <section class="phonebook">
+    <div class="container">
+      <table class="table">
+        <tr class="table-row">
+          <th class="table-heading">Name</th>
+          <th class="table-heading">Surname</th>
+          <th class="table-heading">Number</th>
+          <th class="table-heading">Comment</th>
+        </tr>     
+      </table>
+    </div>
+    </section>
+  </main>
+  
 </body>
 </html>
