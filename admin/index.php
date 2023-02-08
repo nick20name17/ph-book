3079c96adb0fbb0ebca73e8026641782c89831a7
nazar <nick20name17@gmail.com>
2023-02-08 21:07:11 +0200

    adding markup for admin index page and logout logic

--- admin/index.php
+++ admin/index.php
@@ -1,2 +1,44 @@
 <?php
-echo 'index admin';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PhoneBook</title>
+  <link rel="stylesheet" href="../assets/style.css">
+</head>
+<body>
+  <header class="header">
+    <div class="container header__container">
+      <h1 class="title">Phone Book Admin</h1>
+      <a class="header__btn" href="../logout.php">Log out</a>
+    </div>
+  </header>
+
+  <main>
+    <section class="phonebook">
+    <div class="container">
+      <form class="search-form" action="" method="GET">
+        <div class="form-group">
+          <input class="search-input" type="text" name="search" id="search">
+          <input class="search-submit" type="submit" value="Search">
+        </div>
+      </form> 
+
+      <table class="table">
+        <tr class="table-row">
+          <th class="table-heading">Name</th>
+          <th class="table-heading">Surname</th>
+          <th class="table-heading">Number</th>
+          <th class="table-heading">Comment</th>
+        </tr>     
+      </table>
+    </div>
+    </section>
+  </main>
+
+</body>
+</html>
